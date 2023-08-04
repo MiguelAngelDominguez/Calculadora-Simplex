42071441ec48f5cad0a2de17c89a411d8ea1ed06
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-08-04 08:12:27 -0500

    first commit

---
+++ scripts/script_main.js
@@ -0,0 +1,8 @@
+const ctdVariables = document.getElementById();
+const ctdRestric = document.getElementById();
+
+const tableFormulacion = document.getElementById('table');
+
+function createTableFormulacion(ctdVariables,ctdRestric){
+    
+}
