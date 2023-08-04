aa8753c0295ad44a5b10469eb78c3c975665fcb3
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-08-04 08:26:30 -0500

    Se agrego los id's en los html y js para comenzar la logica de formulacion

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -1,8 +1,11 @@
-const ctdVariables = document.getElementById();
-const ctdRestric = document.getElementById();
+const metodoSimp = document.getElementById("MetodoSimplex").value;
+const objetivoSimp = document.getElementById("ObjetivoSimplex").value;
+
+const ctdVariables = document.getElementById("CtdVariables").value;
+const ctdRestric = document.getElementById("ctdRestric").value;
 
 const tableFormulacion = document.getElementById('table');
 
 function createTableFormulacion(ctdVariables,ctdRestric){
-    
+    // more code
 }
