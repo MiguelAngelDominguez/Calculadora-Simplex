e97265729206c2518c7db7bc3d08fd952276de1c
pana-miguel <miguelangel.dominguez@unheval.pe>
2023-08-06 21:50:10 -0500

    Se creo el archivo simplex.js donde incluye la logica del metodo simplex, ademas se importo el nuevo modulo a la modulo principal

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -1,3 +1,5 @@
+    import {imprimirFormulacion, simplexMatricial} from './simplex.js';
+
 const metodoSimp = document.getElementById("MetodoSimplex").value;
 const objetivoSimp = document.getElementById("ObjetivoSimplex").value;
 
@@ -7,24 +9,36 @@ const tableRestric = document.getElementById("tableRestriccion");
 const newInputForm = '<td><input type="number" name="variable" id="variable" class="table-input block w-full rounded-md border-0 py-1.5 pl-7 pr-20 number-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="">x<span>3</span></td>';
 const newRestric = '<td><input type="number" name="variable" id="variable" class="table-input block w-full rounded-md border-0 py-1.5 pl-7 pr-20 number-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="">x<span>3</span></td>';
 
-function createTableFormulacion(){
+function createTableFormulacion() {
     const ctdVariables = document.getElementById("CtdVariables").value;
     const ctdRestric = document.getElementById("ctdRestric").value;
     console.log(ctdVariables);
     console.log(ctdRestric);
     tableFunction.innerHTML += '<th scope="col">Z</th>';
     for (let i = 0; i < ctdVariables; i++) {
-        tableFunction.innerHTML+= newInputForm; 
+        tableFunction.innerHTML += newInputForm;
     }
 
     for (let i = 0; i < ctdRestric; i++) {
-        tableRestric.innerHTML+= '<th scope="row">'+i+'</th>';
+        tableRestric.innerHTML += '<th scope="row">' + i + '</th>';
         var tr = document.createElement('tr');
-        tableFunction.appendChild(tr); 
+        tableFunction.appendChild(tr);
         let compChild = tableRestric.children[i];
         for (let i = 0; i < ctdVariables; i++) {
-            compChild.innerHTML+= newRestric;
+            compChild.innerHTML += newRestric;
         }
-        compChild.innerHTML+= newRestric;
+        compChild.innerHTML += newRestric;
     }
 }
+
+// Ejemplo de uso
+const c = [30, 50];
+const A = [
+    [1, 2],
+    [1, 1],
+];
+const b = [200, 140];
+
+imprimirFormulacion(c, A, b);
+const iterations = simplexMatricial(c, A, b);
+console.log(iterations);
